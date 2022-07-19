2ad5d18ec8c864c49d2ee11e5987261b5e378561
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2022-07-19 16:35:23 +0200

    debug

--- httpdocs/librerias/lib_db.php
+++ httpdocs/librerias/lib_db.php
@@ -1,22 +1,9 @@
 <?php
 
-
-//$hostname_airfarm = "localhost";
-//$database_airfarm = "airfarm_dpo";
-//$username_airfarm = "airfarm_us";
-//$password_airfarm = "Cpt39il8*";
-
-//$airfarm = mysql_pconnect($hostname_airfarm, $username_airfarm, $password_airfarm) or
-
-//trigger_error(mysql_error(),E_USER_ERROR);
-
-
-
-
-function db_connect()
-{
+function db_connect(){
    $result = mysql_pconnect("localhost", "airfarm_us", "Cpt39il8*");
-   if (!$result)
+
+   if ( !$result )
       return false;
    if (!mysql_select_db("airfarm_dpo"))
       return false;
@@ -25,23 +12,7 @@ function db_connect()
 }
 
 
-// function db_connect(){
-//    $result = mysqli_connect("mysql_old_airpharm", "root", "root");
-//
-//    if (!$result){
-//      return false;
-//    }
-//
-//    if (!mysqli_select_db("dbtest")){
-//      return false;
-//    }
-//
-//    return $result;
-// }
-
-
-function db_result_to_array($result)
-{
+function db_result_to_array( $result ){
    $res_array = array();
 
    for ($count=0; $row = @mysql_fetch_array($result); $count++)
